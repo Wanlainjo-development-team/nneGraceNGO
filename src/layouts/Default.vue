433470bd4ffe799711a8002a1602615cf9e75087
rukkiecodes <rukkiecodes@gmail.com>
2023-04-08 02:24:52 +0100

    done with footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,10 +6,13 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <Footer />
   </v-app>
 </template>
 
 <script setup>
 import AppBar from './AppBar.vue'
 import Drawer from "./Drawer.vue";
+import Footer from "./Footer.vue";
 </script>
---
+++ src/layouts/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer class="ma-5 pa-0 d-flex flex-column overflow-hidden" rounded="xl" elevation="5">
+        <Map />
+
+        <div class="px-4 py-2 bg-white text-center w-100">
+            {{ new Date().getFullYear() }} — <strong>Nne-Grace, All Right Reserves | Powered By <span class="text-blue">Wanlainjo</span></strong>
+        </div>
+    </v-footer>
+</template>
+
+<script setup>
+import Map from '@/components/fragments/Map.vue'
+</script>
