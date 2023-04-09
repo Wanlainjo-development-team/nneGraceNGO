9a180308e8ce49e3f89cc7484874b36c55cd1cbd
rukkiecodes <rukkiecodes@gmail.com>
2023-04-09 01:17:58 +0100

    done with basic setup

--- src/layouts/AppBar.vue
+++ src/layouts/AppBar.vue
@@ -1,9 +1,12 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-heart-outline" color="pink-accent-4" size="large" />
+    <v-app-bar-title class="d-flex justify-start">
+      <v-avatar>
+        <v-img src="https://res.cloudinary.com/rukkiecodes/image/upload/v1680885243/Nne-Grace/images/lolo_enij16.jpg"
+          aspect-ratio="1" width="40" />
+      </v-avatar>
 
-      <span class="text-pink-accent-4 font-weight-bold ml-2">Nne</span> <span class="text-blue">Grace</span>
+      <span class="font-weight-bold ml-2">Nne</span>-Grace
     </v-app-bar-title>
 
     <v-spacer />
@@ -13,8 +16,7 @@
       }}</v-btn>
     <v-btn to="/donate" class="bg-blue ml-2">Donate</v-btn>
 
-    <v-app-bar-nav-icon @click="app.drawer = !app.drawer" variant="text"
-        class="hidden-lg-and-up ml-2 rounded-lg" />
+    <v-app-bar-nav-icon @click="app.drawer = !app.drawer" variant="text" class="hidden-lg-and-up ml-2 rounded-lg" />
   </v-app-bar>
 </template>
 
